3a568c585be21a867aabb233b063a772caed3dc2
z18s <z18s@github.com>
2021-02-08 02:31:27 +0300

    Chapter 1

---
+++ src/kotlin_programming_by_example/app.kt
@@ -0,0 +1,11 @@
+package kotlin_programming_by_example
+
+import kotlin_programming_by_example.chapter_1.Hello
+
+fun main(args: Array<String>) {
+
+    // Chapter 1
+    // Hello
+    val hello = Hello();
+    hello.print()
+}
---
+++ src/kotlin_programming_by_example/chapter_1/Hello.kt
@@ -0,0 +1,7 @@
+package kotlin_programming_by_example.chapter_1
+
+class Hello {
+    fun print() {
+        println("Hello, World!")
+    }
+}
